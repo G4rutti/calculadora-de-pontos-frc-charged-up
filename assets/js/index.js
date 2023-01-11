8f9a1a8510e03bb4b053017a42639c8180a706bb
G4rutti <davigarutti5@gmail.com>
2023-01-11 20:50:58 -0300

    logica quase concluida

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,65 @@
 const mobilidade1Auto = document.getElementById("moblilidade-1")
 const pecasDeJogo1Auto = document.getElementById("pecasDoJogo-1Auto")
-const textoPecasDeJogo1Auto = pecasDeJogo1Auto.options[pecasDeJogo1Auto.selectedIndex].value;
 const pecasDeJogo2Auto = document.getElementById("pecasDoJogo-2Auto")
-const textoPecasDeJogo2Auto = pecasDeJogo2Auto.options[pecasDeJogo2Auto.selectedIndex].value;
 const pecasDeJogo3Auto = document.getElementById("pecasDoJogo-3Auto")
-const textoPecasDeJogo3Auto = pecasDeJogo3Auto.options[pecasDeJogo3Auto.selectedIndex].value;
 const encaixadoENaoAcionadoAuto = document.getElementById("encaixadoENaoAcionadoAuto")
 const encaixadoEAcionadoAuto = document.getElementById("encaixadoEAcionadoAuto")
-var ptsChargeStationAuto = 0
+var classificacao = 0
+var ptsChargeStation = 0
+var linksPontuados = 0
+const pecasDeJogo1Tele = document.getElementById("pecasDoJogo-1TeleOp")
+const pecasDeJogo2Tele = document.getElementById("pecasDoJogo-2TeleOp")
+const pecasDeJogo3Tele = document.getElementById("pecasDoJogo-3TeleOp")
+const encaixadoENaoAcionadoTele = document.getElementById("encaixadoENaoAcionadoAuto")
+const encaixadoEAcionadoTele = document.getElementById("encaixadoEAcionadoAuto")
+const parque = document.getElementById('parque')
+const linksFeitos = document.getElementById("3LinksFeito")
+const links = document.getElementById("links")
 
-
+function pegarValor(nomeId){
+    return parseInt(document.getElementById(nomeId).value)
+}
 
 const verificarPontosAutonomo = () => {
     var ptsAutonomo = 0
     if(mobilidade1Auto.checked){
         ptsAutonomo += 3
     }
-    ptsAutonomo += (parseInt(textoPecasDeJogo1Auto) * 3)
-    ptsAutonomo += (parseInt(textoPecasDeJogo2Auto) * 4)
-    ptsAutonomo += (parseInt(textoPecasDeJogo3Auto) * 6)
+    ptsAutonomo += (pecasDeJogo1Auto.value * 3)
+    ptsAutonomo += (pecasDeJogo2Auto.value * 4)
+    ptsAutonomo += (pecasDeJogo3Auto.value * 6)
     if(encaixadoENaoAcionadoAuto.checked){
         ptsAutonomo += 8 
-        ptsChargeStationAuto += 8
+        ptsChargeStation += 8
     }
     if(encaixadoEAcionadoAuto.checked){
         ptsAutonomo += 12
-        ptsChargeStationAuto += 12
+        ptsChargeStation += 12
     }
-    console.log(ptsAutonomo)
+    return ptsAutonomo
 }
 
-
-
+const verificarPontosTeleoperado = () => {
+    var ptsTeleoperado = 0
+    ptsTeleoperado += (pecasDeJogo1Tele.value * 2)
+    ptsTeleoperado += (pecasDeJogo2Tele.value * 3)
+    ptsTeleoperado += (pecasDeJogo3Tele.value * 5)
+    if(linksFeitos.checked){
+        ptsTeleoperado += 5
+    }
+    if(links.value >= 5){
+        classificacao += 1
+    }
+    if(encaixadoENaoAcionadoTele.checked){
+        ptsTeleoperado += 6
+        ptsChargeStation += 6
+    }
+    if(encaixadoEAcionadoTele.checked){
+        ptsTeleoperado += 10
+        ptsChargeStation += 10
+    }
+    if(encaixadoEAcionadoTele.checked){
+        ptsTeleoperado += 2
+    }
+    console.log(ptsTeleoperado)
+}
